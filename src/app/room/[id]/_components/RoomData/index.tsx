f5e0ba0319d839415f36f87d6c7becd83a0b4b91
Tai Porto <tai.porto@pipefy.com>
2023-11-22 19:55:24 -0300

    WIP

--- src/app/room/[id]/_components/RoomData/index.tsx
+++ src/app/room/[id]/_components/RoomData/index.tsx
@@ -1,17 +1,19 @@
-import { Heading, Text, VStack } from '@chakra-ui/react'
-import React from 'react'
+import { Heading, Text, VStack } from "@chakra-ui/react";
+import React from "react";
 
-export const index = () => {
+export const index = ({ room }) => {
   return (
-    <VStack align="center">
-    <Text>Evento:</Text>
-    <Heading as="h2" size="lg" textAlign="center">
-      {room.name}
-    </Heading>
-  </VStack>
-  <VStack align="center">
-    <Text>Limite de valor:</Text>
-    <EditPriceLimitInPlace room={room} />
-  </VStack>
-  )
-}
+    <>
+      <VStack align="center">
+        <Text>Evento:</Text>
+        <Heading as="h2" size="lg" textAlign="center">
+          {room.name}
+        </Heading>
+      </VStack>
+      <VStack align="center">
+        <Text>Limite de valor:</Text>
+        <EditPriceLimitInPlace room={room} />
+      </VStack>
+    </>
+  );
+};
