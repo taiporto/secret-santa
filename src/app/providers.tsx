4719f078f8ea94355811e5e5e04e6284a518d1ba
Tai <tai.dporto@gmail.com>
2024-10-19 14:19:18 -0300

    feat: remove custom font

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,35 +4,20 @@ import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Provider as JotaiProvider } from "jotai";
 
-import { Poppins } from "next/font/google";
-
 import { colors } from "@/theme";
 import { globalStore } from "@/jotai/globalStore";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["100", "300", "400", "600", "700"],
-});
 
 export const theme = extendTheme({
   colors,
   fonts: {
-    heading: "var(--font-poppins)",
-    body: "var(--font-poppins)",
+    heading: "sans-serif",
+    body: "sans-serif",
   },
 });
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <style jsx global>
-        {`
-          :root {
-            --font-poppins: ${poppins.style.fontFamily};
-          }
-        `}
-      </style>
       <CacheProvider>
         <JotaiProvider store={globalStore}>
           <ChakraProvider theme={theme}>{children}</ChakraProvider>
