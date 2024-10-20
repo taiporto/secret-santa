bae6b26b82b5fd80eda6a4e65fe4892017f958ba
Tai <tai.dporto@gmail.com>
2024-10-20 13:42:41 -0300

    fix: remove unused component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-import { Center } from "@chakra-ui/react";
+import {
+  Center,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
 import { MainForm } from "@/components/MainForm";
 import { handleCreateRoom } from "./room/[id]/_utils/handleCreateRoom";
 
@@ -9,7 +16,18 @@ export default async function Home() {
   };
   return (
     <Center>
-      <MainForm handleSubmit={handleSubmit} />
+      <Tabs>
+        <TabList>
+          <Tab>Adicionar participantes</Tab>
+          <Tab>Copia e cola</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <MainForm handleSubmit={handleSubmit} />
+          </TabPanel>
+          <TabPanel></TabPanel>
+        </TabPanels>
+      </Tabs>
     </Center>
   );
 }
