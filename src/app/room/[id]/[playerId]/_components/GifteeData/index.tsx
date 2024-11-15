f72726cde97f20eb795967c503e167f1be30ee37
Tai <tai.dporto@gmail.com>
2024-11-14 22:29:22 -0300

    feat: add amazon button

--- src/app/room/[id]/[playerId]/_components/GifteeData/index.tsx
+++ src/app/room/[id]/[playerId]/_components/GifteeData/index.tsx
@@ -8,6 +8,7 @@ import {
   Heading,
   VStack,
   Box,
+  Button,
 } from "@chakra-ui/react";
 import { GifteeCard } from "../GifteeCard";
 import {
@@ -16,6 +17,7 @@ import {
   Wishlist,
 } from "../../../../../../../types";
 import { WishlistItem } from "../MyWishlist/components/WishlistItem";
+import { FaAmazon } from "react-icons/fa";
 
 export const GifteeData = ({
   giftee,
@@ -26,35 +28,47 @@ export const GifteeData = ({
 }) => {
   return (
     <VStack gap={4} align={{ base: "center", md: "left" }}>
-      <Heading as="h3" size="md">
+      <Heading as="h3" size="md" textAlign={"center"}>
         Veja quem você sorteou abaixo:
       </Heading>
       <GifteeCard giftee={giftee} />
-      {!!gifteesWishlist?.length && (
-        <Accordion allowToggle>
-          <AccordionItem>
-            <AccordionButton>
-              Lista de presentes do sorteado
-              <AccordionIcon />
-            </AccordionButton>
-            <AccordionPanel>
-              {gifteesWishlist.map((item: TWishlistItem) => {
-                return (
-                  <Box key={item.id}>
-                    <Box my="4">
-                      <WishlistItem
-                        wishlistItemData={item}
-                        allowDelete={false}
-                      />
+      <Box mt={2}>
+        {!!gifteesWishlist?.length && (
+          <Accordion allowToggle>
+            <AccordionItem>
+              <AccordionButton>
+                Lista de presentes do sorteado
+                <AccordionIcon />
+              </AccordionButton>
+              <AccordionPanel>
+                {gifteesWishlist.map((item: TWishlistItem) => {
+                  return (
+                    <Box key={item.id}>
+                      <Box my="4">
+                        <WishlistItem
+                          wishlistItemData={item}
+                          allowDelete={false}
+                        />
+                      </Box>
+                      <Divider />
                     </Box>
-                    <Divider />
-                  </Box>
-                );
-              })}
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      )}
+                  );
+                })}
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        )}
+        <Button
+          mt={5}
+          variant="outline"
+          leftIcon={<FaAmazon />}
+          as="a"
+          href="https://www.amazon.com.br?&linkCode=ll2&tag=amigooculto-20&linkId=6ea28ef9c399b868ce8e1ec36b84b367&language=pt_BR&ref_=as_li_ss_tl"
+          target="_blank"
+        >
+          Comprar presente na Amazon
+        </Button>
+      </Box>
     </VStack>
   );
 };
